d0eaccbc9d5b33c91cb6caa995fb8a3d188988fe
BennyZHill <ben.h@builtbypixel.com>
2021-08-23 12:11:42 +0100

    added username

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.status(200).send("This is the about route");
 });
+app.get("/users/:username", (req, res) => {
+  res
+    .status(200)
+    .send(`You requested information about ${req.params.username}`);
+});
 
 app.listen(port, () => {
   console.log("App is online");
