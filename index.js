c0bb089f677e50f860c7ad9b36b0a0a2d96c7aee
BennyZHill <ben.h@builtbypixel.com>
2021-08-23 12:32:23 +0100

    added servers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world");
