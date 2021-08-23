d4e061dbc0248073a011cda903103617c1fa43c7
BennyZHill <ben.h@builtbypixel.com>
2021-08-23 15:31:12 +0100

    node start

--- index.js
+++ index.js
@@ -3,30 +3,35 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express.json());
+
+app.use(express.json()); // This ensures input is considered to be json
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello world");
 });
 
-app.get("/about", (req, res) => {
-  res.status(200).send("This is the about route");
-});
-
-app.get("/users/:username", (req, res) => {
-  console.log(req.query);
+app.post("/:username/", (req, res) => {
   res
-    .status(200)
-    .send(
-      `You requested information about ${req.params.username}: ${req.query.age}`
-    );
+    .status(201)
+    .json({
+      message: `You created the repo ${req.body.project}`,
+      data: req.body,
+    });
 });
 
 app.get("/:username/:project", (req, res) => {
   res
     .status(200)
-    .send(
-      `You requested information about ${req.params.project} created by ${req.params.username}`
-    );
+    .json({ message: `You views the project ${req.params.project}` });
+});
+
+app.post("/:username/:project", (req, res) => {
+  res
+    .status(200)
+    .json({
+      message: `You updated the project: ${req.params.project}`,
+      date: req.body,
+    });
 });
 
 app.listen(port, () => {
