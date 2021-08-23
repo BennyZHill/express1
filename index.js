6e8aea75e44018ddc4c301f3ec7fca52da2e3c0e
BennyZHill <ben.h@builtbypixel.com>
2021-08-23 12:07:05 +0100

    modified index

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello world");
+  res.status(200).send("Hello world");
 });
+
+app.get("/about", (req, res) => {
+  res.status(200).send("This is the about route");
+});
+
 app.listen(port, () => {
   console.log("App is online");
 });
