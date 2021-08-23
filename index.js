7c331047ba97a6c2279a309028d8d1ab2332f8b1
BennyZHill <ben.h@builtbypixel.com>
2021-08-23 14:54:55 +0100

    commit -m "added express.use"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send("Hello world");
 });
